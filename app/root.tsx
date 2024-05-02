81d82bece74305925bbf359f535adc4c9fdca624
1willcobb <cobb.will@gmail.com>
2024-05-01 22:10:03 -0700

    remi

--- app/root.tsx
+++ app/root.tsx
@@ -1,13 +1,25 @@
+import type { LoaderFunctionArgs } from "@remix-run/node";
 import {
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData
 } from "@remix-run/react";
 import { Analytics } from "@vercel/analytics/react";
 
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+
+  
+  return null;
+};
+
+
 export function Layout({ children }: { children: React.ReactNode }) {
+  const data = useLoaderData()
+
+
   return (
     <html lang="en">
       <head>
